25e30a2f34a2f545dc3836e42a1cf9d96570fea7
Juan Jose Vergara <40677444+jjvemz@users.noreply.github.com>
2024-10-10 17:18:02 -0300

    nueva seguridad para los endpoints

--- bcknd/backend/src/main/java/com/ecommerce/backend/api/security/WebSecurityConfig.java
+++ bcknd/backend/src/main/java/com/ecommerce/backend/api/security/WebSecurityConfig.java
@@ -4,14 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+
+import com.auth0.jwt.JWT;
 
 @Configuration
 public class WebSecurityConfig {
     
+    private JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter){
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable());
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
+        
+        http.authorizeHttpRequests(authorize -> authorize
+        // Exclusiones para las autenticaciones
+        .requestMatchers("/product", "/auth/register", "/auth/login").permitAll()
+        // Endpoints protegidos con la autenticación
+        .anyRequest().authenticated()
+    );
         return http.build();
     }
 }
